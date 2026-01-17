580e63bcbbd16a29f303555ee23a4c9a0e87ea74
QUYNHQUEEN <nguyenanhcgnt1204@gmail.com>
2026-01-17 22:28:34 +0700

    use ngIf-else in component

--- src/app/user-card/userList.ts
+++ src/app/user-card/userList.ts
@@ -17,26 +17,12 @@ export const userList: User[] = [
     {
         "id": 2,
         "name": "Quỳnh Anh",
-        "age": 20,
+        "age": 19,
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "avatar": "assets/img/avatar.jpg"
     },
     {
         "id": 3,
-        "name": "Quỳnh Anh",
-        "age": 20,
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        "avatar": "assets/img/avatar.jpg"
-    },
-    {
-        "id": 4,
-        "name": "Quỳnh Anh",
-        "age": 20,
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        "avatar": "assets/img/avatar.jpg"
-    },
-    {
-        "id": 5,
         "name": "Quỳnh Anh",
         "age": 20,
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
