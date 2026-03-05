10a530fb0c8b0bbf1a04a34c94ba65151644d32d
QUYNHQUEEN <nguyenanhcgnt1204@gmail.com>
2026-03-05 16:13:50 +0700

    doc: standalone-component

---
+++ Documents/Standalone-component/standalone.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'standalone',
+    template: `
+        <p>Standalone component example</p>
+    `
+})
+
+export class StandaloneComponent {}
